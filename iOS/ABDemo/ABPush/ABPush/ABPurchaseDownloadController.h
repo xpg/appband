cf78f26dbe037f1d0d2a6590fbb17f8542ac245b
Jason Wang <wl19830119@gmail.com>
2011-11-27 14:53:28 +0800

    Implement In app Purchase record function

---
+++ iOS/ABDemo/ABPush/ABPush/ABPurchaseDownloadController.h
@@ -0,0 +1,13 @@
+//
+//  ABPurchaseDownloadController.h
+//  ABPush
+//
+//  Created by Jason Wang on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABPurchaseDownloadController : UIViewController
+
+@end
---
+++ iOS/AppBand/AppBand/Library/Common/ABDownloadRequest.m
@@ -0,0 +1,13 @@
+//
+//  ABDownloadRequest.m
+//  AppBand
+//
+//  Created by Jason Wang on 11/27/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ABDownloadRequest.h"
+
+@implementation ABDownloadRequest
+
+@end
